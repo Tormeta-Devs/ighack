3af2eee7378e07908e9af6505dc3abe5164c2eb3
JuanEGamerYT <99984511+JuanEGamerYT@users.noreply.github.com>
2023-04-05 14:31:50 -0300

    Fix

--- setup.sh
+++ setup.sh
@@ -2,6 +2,11 @@ clear
 sleep 2
 echo -e "Installing"
 sleep 1
+
+cd /data/data/com.termux/files/usr/etc/tor
+#if [[ -f "torrc" ]]
+#then
+
 pkg update -y
 pkg upgrade -y
 apt update -y
@@ -26,6 +31,12 @@ rm -rf torrc
 cd $HOME/ighack
 cp -r torrc /data/data/com.termux/files/usr/etc/tor
 cd
+tor
+
+
+#else
+#cd /data/data/com.termux/files/home/xinsta_brute
+#cp -r torrc /data/data/com.termux/files/usr/etc/
 echo "   "
 clear
 echo "Ante de usar el programa revise que en otra terminal este ejecutandose tor"
